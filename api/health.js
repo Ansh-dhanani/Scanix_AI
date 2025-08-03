faf78a5c2d5cd98b2941509041206ba29db72927
Ansh-Dhanani <dhananiansh01@gmail.com>
2025-08-03 11:10:10 +0530

    Remove JS APIs, use Python Flask API only

--- api/health.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-  return res.status(200).json({ status: 'API is running' });
-}
